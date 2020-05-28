f72bd02ef9c7ac08c9ecd7de1501efa34b0cb0f6
Alexander Shteyngolts <shtein@gmail.com>
2020-05-28 15:40:56 -0700

    deleting wrong files

--- LedEffects.ino.cpp
+++
@@ -1,9 +0,0 @@
-# 1 "/var/folders/n1/bv4jkqdj3gl_1xqcpyj21lgxk0015f/T/tmpMoah9g"
-#include <Arduino.h>
-# 1 "/Users/alexs/Documents/Arduino/LED/LedEffects/LedEffects.ino"
-# 17 "/Users/alexs/Documents/Arduino/LED/LedEffects/LedEffects.ino"
-#include "setups.h"
-void loop();
-#line 20 "/Users/alexs/Documents/Arduino/LED/LedEffects/LedEffects.ino"
-void loop(){
-}
